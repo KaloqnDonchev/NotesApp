3a11fb86f51fc2e9007dcb317b601a33defa2c27
Kaloyan <kaloqndonchev99@gmail.com>
2024-04-27 16:07:44 +0300

    basic structure and design

--- notes-app-frontend/vite-project/src/App.jsx
+++ notes-app-frontend/vite-project/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "Note 1",
+      description: "Description 1",
+    },
+    {
+      id: 2,
+      title: "Note 2",
+      description: "Description 2",
+    },
+    {
+      id: 3,
+      title: "Note 3",
+      description: "Description 3",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      description: "Description 4",
+    },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="Title" required></input>
+        <textarea placeholder="Description" rows={8} required></textarea>
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) => (
+        <div className="note-item" key={note.id}>
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>{note.title}</h2>
+          <p>{note.description}</p>
+        </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
