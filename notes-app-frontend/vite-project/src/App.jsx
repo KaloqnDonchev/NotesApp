31acefcbe98b8ab3a83898a594ee4a426b0a2150
Kaloyan <kaloqndonchev99@gmail.com>
2024-04-28 00:22:55 +0300

    connect backend with frontend logic

--- notes-app-frontend/vite-project/src/App.jsx
+++ notes-app-frontend/vite-project/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css"
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      title: "Note 1",
-      description: "Description 1",
-    },
-    {
-      id: 2,
-      title: "Note 2",
-      description: "Description 2",
-    },
-    {
-      id: 3,
-      title: "Note 3",
-      description: "Description 3",
-    },
-    {
-      id: 4,
-      title: "Note 4",
-      description: "Description 4",
-    },
-  ]);
-
+  const [notes, setNotes] = useState([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [selectedNote, setSelectedNote] = useState(null);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/notes");
+
+        const notes = await response.json();
+
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNotes();
+  }, []);
 
   const handleNoteClick = (note) => {
     setSelectedNote(note);
@@ -35,56 +29,86 @@ const App = () => {
     setDescription(note.description);
   }
 
-  const handleAddNote = (e) => {
+  const handleAddNote = async (e) => {
     e.preventDefault();
 
-    const newNote = {
-      id: notes.length + 1,
-      title: title,
-      description: description,
-    };
+    try {
+      const response = await fetch("http://localhost:8080/api/notes", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            description
+          }),
+      });
 
-    setNotes([newNote, ...notes]);
-    setTitle("");
-    setDescription("");
+      const newNote = await response.json();
+
+      setNotes([newNote, ...notes]);
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      console.error(error);
+    }
+
   };
 
-  const handleUpdateNote = (e) => {
+  const handleUpdateNote = async (e) => {
     e.preventDefault();
 
     if(!selectedNote) {
       return;
     };
 
-    const updatedNote = {
-      id: selectedNote.id,
-      title: title,
-      description: description,
-    };
+    try {
+      const response = await fetch(`http://localhost:8080/api/notes/${selectedNote.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description
+        }),
+      });
 
-    const updatedNotesList = notes.map((note) => {
-      note.id === selectedNote.id ? updatedNote : note;
-    });
+      const updatedNote = await response.json();
 
-    setNotes(updatedNotesList);
-    setTitle("");
-    setDescription("");
-    setSelectedNote(null);
+      const updatedNotesList = notes.map((note) => {
+        note.id === selectedNote.id ? updatedNote : note;
+      });
+  
+      setNotes(updatedNotesList);
+      setTitle("");
+      setDescription("");
+      setSelectedNote(null);
+    } catch (error) {
+      console.error(error);
+    }
+
   };
 
-  const handleDeleteNote = (event, noteId) => {
+  const handleDeleteNote = async (event, noteId) => {
     event.stopPropagation();
 
-    const updatedNotes = notes.filter((note) => note.id !== noteId);
-
-    setNotes(updatedNotes);
-  }
+    try {
+      await fetch(`http://localhost:8080/api/notes/${noteId}`, {
+        method: "DELETE",
+      })
+      const updatedNotes = notes.filter((note) => note.id !== noteId);
+      setNotes(updatedNotes);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleCancel = () => {
     setTitle("");
     setDescription("");
     setSelectedNote(null);
-  }
+  };
 
   return (
     <div className="app-container">
